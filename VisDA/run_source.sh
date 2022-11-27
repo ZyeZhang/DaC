79c662bf9ac80712c20510c28c5449bf9563ea66
zhangziyi <zhangziyi@lamda.nju.edu.cn>
2022-11-27 08:37:00 +0000

    visda

--- VisDA/run_source.sh
+++ VisDA/run_source.sh
@@ -1,1 +1,1 @@
-python image_source.py --trte val --output experiments/VISDA-C/source/ --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 10 --s 0
+python source.py --trte val --output experiments/VISDA-C/source/ --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 10 --s 0
